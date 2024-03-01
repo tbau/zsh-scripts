534a9194188a0c06021fa91437f308dc0886378e
Thomas Bau <tbau11508@gmail.com>
2024-03-01 01:15:52 -0600

    Added function for a custom git blame

--- src/git/git-scripts.sh
+++ src/git/git-scripts.sh
@@ -11,6 +11,11 @@ gp(){
     git pull
 }
 
+# Push branch
+gpsh(){
+    git push
+}
+
 # Git checkout
 gc(){
     git checkout $1
@@ -78,12 +83,12 @@ gbd(){
 }
 
 # List the all branches
-gb(){
+gbr(){
     git branch
 }
 
 # List the current git branch
-cb(){
+cbr(){
     git rev-parse --abbrev-ref HEAD
 }
 
@@ -110,6 +115,64 @@ gclog(){
     &emsp;&emsp;&emsp;&emsp;**Date**: %m/%d/%y<br>
     &emsp;&emsp;&emsp;&emsp;**Time**: %H:%M<br>
     &emsp;&emsp;&emsp;&emsp;**Timezone**: %z"  >> changelog.md
+}
+
+# Git blame
+gbl(){
+    filename=$1
+    local start_line=${2:-1}  # Default start line is 1 if not provided
+    local end_line=${3:-999999}
+    # Check if a filename is provided
+    if [ -z "$filename" ]; then
+        echo "Usage: $0 <filename>"
+        exit 1
+    fi
+
+    # Use git blame to get blame information for the entire file
+    blame_output=$(git blame --line-porcelain "$filename" 2>/dev/null)
+
+    # Initialize variables to store commit information
+  
+    line_num=1;
+    line_count=0
+
+    # Iterate over each line in the blame output
+    for line in "${(@f)blame_output}"; do
+        # if [[ "$line_num" -lt "$start_line "]]; then
+        #     ((line_num++))
+        # fi
+        # Check if the line contains the commit hash and line number
+        if [[ "$line" =~ ^([0-9a-f]{40})\ ([0-9]+)(\ [0-9]+)?\ ([0-9]+)$ ]]; then
+            # Extract commit hash and line number
+            commit_hash="${match[1]}"        
+            short_commit_hash=$(git rev-parse --short "$commit_hash" 2>/dev/null)
+            short_commit_hash=${short_commit_hash/0000000/New}
+        fi
+        # Check if the line contains the author information
+        if [[ "$line" =~ ^author\ (.*)$ ]]; then
+            # Extract author
+            author=$(sed 's/Not Committed Yet//' <<< "${match[1]}")
+            author=${author:0:20}
+        fi
+        # Check if the line contains the summary
+        if [[ "$line" =~ ^summary\ (.*)$ ]]; then
+            # Extract summary
+            summary=$(sed '/Version of/ s/.*/ /' <<< "${match[1]}")
+            summary=${summary:0:60}
+        fi
+        # Check if the line contains the line content
+        if [[ "$content_line" == "1" ]]; then
+            # Print the commit information for the line
+            printf "Line %-5s%-9s%-22s%-60s%-20s\n" "$line_num" "$short_commit_hash" "$author" "$summary" "$line"
+            line_count=0
+            content_line="0"
+            ((line_num++))
+        fi
+        if [[ "$line" =~ ^filename\ (.*)$ ]]; then
+            content_line="1"
+        fi
+        ((line_count++))
+    done
 }
 
 # Downloads list of repositories from a file
@@ -139,6 +202,7 @@ source "$(dirname "${zsh_scripts_directories["git_scripts_dir"]}")/shared/shared
 
 documentCommand "git" "commits" "gs" "Get status of modified and committed files"
 documentCommand "git" "branches" "gp" "Pull branch from remote repository"
+documentCommand "git" "branches" "gpsh" "Push branch to remote repository"
 documentCommand "git" "branches" "gc" "Checkout an existing branch"
 documentCommand "git" "commits" "report" "gl" "Print commit history with graphical branches"
 documentCommand "git" "repos" "gcr" "Clone remote repository"
@@ -147,7 +211,8 @@ documentCommand "git" "branches" "gsw" "Switch to previous branch"
 documentCommand "git" "branches" "gmd" "Merge dev into current branch"
 documentCommand "git" "branches" "gcb" "Checkout new branch"
 documentCommand "git" "branches" "gbd" "Delete local branch"
-documentCommand "git" "branches" "gb" "Print all branches"
-documentCommand "git" "branches" "cb" "Print current branch"
+documentCommand "git" "branches" "gbr" "Print all branches"
+documentCommand "git" "branches" "cbr" "Print current branch"
 documentCommand "git" "commits" "report" "gclog" "Output git commit changelog to file"
+documentCommand "git" "commits" "report" "gbl" "Outputs custom git blame for file"
 documentCommand "git" "repos" "cloneRepos" "Clones list of repositories from a file"
